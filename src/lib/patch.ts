877493daa183ee85cba54d71111e109b6c43a48b
patomation <patomation@gmail.com>
2020-08-17 22:40:12 +0700

    refactor(snabbdom): moves patch init into its own module

---
+++ src/lib/patch.ts
@@ -0,0 +1,12 @@
+import { init } from 'snabbdom/init'
+import { classModule } from 'snabbdom/modules/class'
+import { propsModule } from 'snabbdom/modules/props'
+import { styleModule } from 'snabbdom/modules/style'
+import { eventListenersModule } from 'snabbdom/modules/eventlisteners'
+
+export const patch = init([ // Init patch function with chosen modules
+  classModule, // makes it easy to toggle classes
+  propsModule, // for setting properties on DOM elements
+  styleModule, // handles styling on elements with support for animations
+  eventListenersModule // attaches event listeners
+])
