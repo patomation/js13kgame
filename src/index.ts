31d36b494e06962886915d55c61a36a15b545aab
patomation <patomation@gmail.com>
2020-08-20 20:31:01 +0700

    refactor: main index file just to have main game logic connections

--- src/index.ts
+++ src/index.ts
@@ -1,80 +1,11 @@
-import { patch } from './lib/patch'
-import { h } from 'snabbdom/h' // helper function for creating vnodes
-import { VNode } from 'snabbdom/vnode'
 import { initCanvas, resizeCanvas, addImage } from './lib/canvas'
 import { Sprite } from './classes/Sprite'
 import ufo from '../assets/sprites/ufo.png'
 import { up, down, left, right } from './lib/input'
 
-import { Modal } from './components/Modal'
-
-interface State {
-  score: number,
-  showStartScreen: boolean
-}
-
-let state: State = {
-  score: 5000,
-  showStartScreen: true
-}
-
-const view = ({
-  score, showStartScreen
-}: State) => h('section', {
-  style: {
-    color: '#fff',
-    height: '100%',
-    display: 'flex',
-    flexDirection: 'column'
-  }
-}, [
-  h('nav', {
-    style: {
-      background: 'purple'
-    }
-  }, [
-    h('div', [
-      h('span', 'score'),
-      h('span', {}, score),
-      'hud',
-      h('button', {
-        on: {
-          click: () => setState({ score: state.score + 1000 })
-        }
-      }, 'get points'),
-      h('button', {
-        on: {
-          click: () => setState({ showModal: true })
-        }
-      }, 'showModal')
-    ])
-  ]),
-  h('main', {
-    style: {
-      flexGrow: '1'
-    }
-  }),
-  showStartScreen
-    ? Modal({}, [
-      h('h1', 'Game Title'),
-      h('button', { on: { click: () => setState({ showStartScreen: false }) } }, 'Start')
-    ]) : null
-])
-
-function setState (newState: Record<string, unknown>) {
-  state = { ...state, ...newState }
-  render()
-}
-
-let vnode: VNode
-
-function render () {
-  vnode = patch(vnode, view(state))
-}
+import { render } from './render'
 
 window.addEventListener('DOMContentLoaded', () => {
-  const container = document.getElementById('container') as Element
-  vnode = patch(container, view(state))
   render()
 
   initCanvas()
