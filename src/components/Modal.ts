a25dddaa8856fc7f5ed539a5277a5432fbc25a72
patomation <patomation@gmail.com>
2020-08-20 23:47:33 +0700

    refactor(components): modal takes style prop

--- src/components/Modal.ts
+++ src/components/Modal.ts
@@ -3,27 +3,32 @@ import { VNodeStyle } from 'snabbdom/modules/style'
 import { VNode } from 'snabbdom/vnode'
 
 export interface Props {
-  click?: EventListener
+  click?: EventListener,
+  style?: VNodeStyle
 }
 
 export const Modal = ({
+  style,
   click
 }: Props,
 children?: ArrayOrElement<VNodeChildElement>
 ): VNode => h('div', {
   props: { className: 'Modal' },
   style: {
-    position: 'fixed',
-    top: '0',
-    left: '0',
-    width: '100%',
-    height: '100%',
-    background: '#222',
-    opacity: '0',
-    transition: 'opacity 1s',
-    delayed: { opacity: '1' },
-    remove: { opacity: '0' }
-  } as unknown as VNodeStyle
+    ...{
+      position: 'fixed',
+      top: '0',
+      left: '0',
+      width: '100%',
+      height: '100%',
+      background: '#222',
+      opacity: '0',
+      transition: 'opacity 500ms',
+      delayed: { opacity: '1' },
+      remove: { opacity: '0' }
+    } as unknown as VNodeStyle,
+    ...style as unknown as VNodeStyle
+  }
 }, [
   h('div', {
     props: { className: 'Modal__content' },
